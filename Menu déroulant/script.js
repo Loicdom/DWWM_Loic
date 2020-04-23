454331760590aa985374d2cbe72e29e32e62b2c8
Loicdom <loicdomDev@gmail.com>
2020-04-23 08:54:19 +0200

    V7

--- Menu déroulant/script.js
+++ Menu déroulant/script.js
@@ -1,3 +1,10 @@
-menu = document.getElementById("menuP");
-menu = document.getElementById("menuP2");
+var menu = document.getElementsByClassName("menu");
+var sousMenu = document.getElementsByClassName("sousMenu");
+for (i=0;i<lesMenus.length;i++) {
+    lesMenus[i].addEventListener("click",GestionSousMenu);
+}
 
+function GestionSousMenu() 
+{
+    
+}
