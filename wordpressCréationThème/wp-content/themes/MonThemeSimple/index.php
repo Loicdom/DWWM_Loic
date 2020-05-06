48a4586c5620d689f6b1a454c1e9a4fb2dc1d96e
Loicdom <loicdomDev@gmail.com>
2020-05-06 12:29:24 +0200

    V21 Creation theme

--- wordpressCréationThème/wp-content/themes/MonThemeSimple/index.php
+++ wordpressCréationThème/wp-content/themes/MonThemeSimple/index.php
@@ -1,7 +1,12 @@
-<!DOCTYPE html> 
-<html lang="fr"> 
-<head> 
-<meta charset="utf-8"> 
-<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
-<meta name="viewport" content="width=device-width, initial-scale=1"> 
-<meta name="description" content=""> <meta name="author" content=""> <title>Exemple de thème WordPress</title> 
+    <?php get_header(); ?>    
+        <div class="row">
+            <div class="col-sm-8 blog-main">
+            <?php    
+            if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+            get_template_part( 'content', get_post_format() ); 
+            endwhile; endif; 
+            ?>
+            </div><!-- /.blog-main -->
+            <?php get_sidebar(); ?> 
+        </div><!-- /.row -->
+        <?php get_footer(); ?> 
