443f03823e52fe523dbfbc82a6d81bd582585602
Loicdom <loicdomDev@gmail.com>
2020-05-06 19:44:28 +0200

    MAJ CreationDuTheme

--- wordpressSiteBasket/wp-content/wflogs/config.php
+++ wordpressSiteBasket/wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1588775435;s:7:"authKey";s:64:"q2&,@OU(` wUU]YU3F,6K~!t?.mp/0(iXE9,rLwb=2-D2I/kK/A2Q:+CZ5#vG-UT";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2848;}
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"q2&,@OU(` wUU]YU3F,6K~!t?.mp/0(iXE9,rLwb=2-D2I/kK/A2Q:+CZ5#vG-UT";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2848;}
