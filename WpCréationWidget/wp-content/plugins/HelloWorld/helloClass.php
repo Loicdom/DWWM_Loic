8f00e300b497f8786ebd8163485e638d2c60212b
Loicdom <loicdomDev@gmail.com>
2020-05-12 15:19:22 +0200

    MAJ Plugin NomCom BDD

--- WpCréationWidget/wp-content/plugins/HelloWorld/helloClass.php
+++ WpCréationWidget/wp-content/plugins/HelloWorld/helloClass.php
@@ -17,22 +17,23 @@ class HelloClass{
     public static function install() 
     {//méthode déclenchée à l'activation du plug-in     
         global $wpdb;     
-        $wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}testcomment (id INT AUTO_INCREMENT PRIMARY KEY, comments VARCHAR(255) NOT NULL);"); 
+        $wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}test (id INT AUTO_INCREMENT PRIMARY KEY, nom VARCHAR(255) NOT NULL, commentaire VARCHAR(255) NOT NULL);"); 
     }
     public static function uninstall()  
     {//méthode déclenchée à la suppression du module   
         global $wpdb;   
-        $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}testcomment;");  
+        $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}test;");  
     }
     public function save_comm() 
     {  
-        if (isset($_POST['helloworld_comm']) && !empty($_POST['helloworld_comm']))   
+        if (isset($_POST['helloworld_comm']) && !empty($_POST['helloworld_comm'] || isset($_POST['helloworld_nom']) && !empty($_POST['helloworld_nom'])))   
         {
             global $wpdb;
             $comm = $_POST['helloworld_comm'];
-            $row = $wpdb->get_row("SELECT * FROM {$wpdb->prefix}testcomment WHERE comments = '$comm'");
+            $nom = $_POST['helloworld_nom'];
+            $row = $wpdb->get_row("SELECT * FROM {$wpdb->prefix}test WHERE commentaire = '$comm' AND nom = '$nom'");
             if (is_null($row)) {    
-                $wpdb->insert("{$wpdb->prefix}testcomment", array('comments' => $comm));   
+                $wpdb->insert("{$wpdb->prefix}test", array('commentaire' => $comm, 'nom' => $nom));   
             }  
         } 
     } 
--- WpCréationWidget/wp-content/plugins/HelloWorld/helloWidget.php
+++ WpCréationWidget/wp-content/plugins/HelloWorld/helloWidget.php
@@ -1,28 +1,32 @@
-<?php 
-class hellowidget extends WP_Widget 
-{  
-    public function __construct()     
-    {         
-        parent::__construct('helloworld', 'Hello World', array('description' => 'Un plug-in qui écrit Hello World'));     
-    }  
-    public function widget($args, $instance)  
-    { // formulaire afficher à l'écran pour l'utilisateur 
- 
-    // on appel les méthodes standard au cas où un autre plug-in les aurait surchargées     
-    echo $args['before_widget'];     
-    echo $args['before_title'];     
-    echo apply_filters('widget_title', $instance['title']);     
-    echo $args['after_title'];     
-    // corps du widget  
-    ?>     
-    <form action="" method="post">
-        <p>
-            <label for="helloworld_comm">Votre commentaire :</label>
-            <input id="helloworld_comm" name="helloworld_comm" type="texte"/>
-        </p>
-        <input type="submit"/>
-    </form>
-    <?php     
-    echo $args['after_widget'];  
+<?php
+class hellowidget extends WP_Widget
+{
+    public function __construct()
+    {
+        parent::__construct('helloworld', 'Hello World', array('description' => 'Un plug-in qui écrit Hello World'));
     }
-} 
+    public function widget($args, $instance)
+    { // formulaire afficher à l'écran pour l'utilisateur 
+
+        // on appel les méthodes standard au cas où un autre plug-in les aurait surchargées     
+        echo $args['before_widget'];
+        echo $args['before_title'];
+        echo apply_filters('widget_title', $instance['title']);
+        echo $args['after_title'];
+        // corps du widget  
+?>
+        <form action="" method="post">
+            <p>
+                <label for="helloworld_nom">Votre Nom :</label>
+                <input id="helloworld_nom" name="helloworld_nom" type="texte" />
+            </p>
+            <p>
+                <label for="helloworld_comm">Votre commentaire :</label>
+                <input id="helloworld_comm" name="helloworld_comm" type="texte" />
+            </p>
+            <input type="submit" />
+        </form>
+<?php
+        echo $args['after_widget'];
+    }
+}
