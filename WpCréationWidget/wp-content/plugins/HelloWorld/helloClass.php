385ddedb0727715b3ad4cfa08895ffc95c787501
Loicdom <loicdomDev@gmail.com>
2020-05-12 18:28:57 +0200

    MAJ exo17 plug in

--- WpCréationWidget/wp-content/plugins/HelloWorld/helloClass.php
+++ WpCréationWidget/wp-content/plugins/HelloWorld/helloClass.php
@@ -1,40 +1,77 @@
-<?php 
+<?php
 //on inclu la definition du widget 
-include_once plugin_dir_path( __FILE__ ).'/hellowidget.php'; 
- 
-class HelloClass{  
-    public function __construct(){   
+include_once plugin_dir_path(__FILE__) . '/hellowidget.php';
+
+class HelloClass
+{
+    public function __construct()
+    {
         // on déclare le widget   
-        add_action('widgets_init', function(){register_widget('HelloWidget');});  
-        add_action('wp_enqueue_scripts',array($this,'persoCSS'),15); 
+        add_action('widgets_init', function () {
+            register_widget('HelloWidget');
+        });
+        add_action('wp_enqueue_scripts', array($this, 'persoCSS'), 15);
         // on ajoute l'action de sauvegarde au chargement du widget   
-        add_action('wp_loaded', array($this, 'save_comm')); 
-    } 
-    function persoCSS() 
-    {  
-        wp_enqueue_style('Hellocss', plugins_url('helloworld/design.css')); 
+        add_action('wp_loaded', array($this, 'save_comm'));
+        add_action('admin_menu', array($this, 'add_admin_menu'), 20);
+        add_action('admin_init', array($this, 'register_settings')); 
     }
-    public static function install() 
-    {//méthode déclenchée à l'activation du plug-in     
-        global $wpdb;     
-        $wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}test (id INT AUTO_INCREMENT PRIMARY KEY, nom VARCHAR(255) NOT NULL, commentaire VARCHAR(255) NOT NULL);"); 
+    function persoCSS()
+    {
+        wp_enqueue_style('Hellocss', plugins_url('helloworld/design.css'));
     }
-    public static function uninstall()  
-    {//méthode déclenchée à la suppression du module   
-        global $wpdb;   
-        $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}test;");  
+    public static function install()
+    { //méthode déclenchée à l'activation du plug-in     
+        global $wpdb;
+        $wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}commentaire (id INT AUTO_INCREMENT PRIMARY KEY, comments VARCHAR(255) NOT NULL);");
+        $wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}testNom (id INT AUTO_INCREMENT PRIMARY KEY, nom VARCHAR(255) NOT NULL);");
     }
-    public function save_comm() 
-    {  
-        if (isset($_POST['helloworld_comm']) && !empty($_POST['helloworld_comm'] || isset($_POST['helloworld_nom']) && !empty($_POST['helloworld_nom'])))   
-        {
+    public static function uninstall()
+    { //méthode déclenchée à la suppression du module   
+        global $wpdb;
+        $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}commentaire;");
+        $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}testNom;");
+    }
+    public function save_comm()
+    {
+        if (isset($_POST['helloworld_comm']) && !empty($_POST['helloworld_comm'] || isset($_POST['helloworld_nom']) && !empty($_POST['helloworld_nom']))) {
             global $wpdb;
             $comm = $_POST['helloworld_comm'];
             $nom = $_POST['helloworld_nom'];
-            $row = $wpdb->get_row("SELECT * FROM {$wpdb->prefix}test WHERE commentaire = '$comm' AND nom = '$nom'");
-            if (is_null($row)) {    
-                $wpdb->insert("{$wpdb->prefix}test", array('commentaire' => $comm, 'nom' => $nom));   
-            }  
-        } 
-    } 
-} 
+            $rowCom = $wpdb->get_row("SELECT * FROM {$wpdb->prefix}commentaire WHERE comments = '$comm'");
+            $rowNom = $wpdb->get_row("SELECT * FROM {$wpdb->prefix}testNom WHERE nom = '$nom'");
+            if (is_null($rowCom)) {
+                $wpdb->insert("{$wpdb->prefix}commentaire", array('comments' => $comm));
+            }
+            if (is_null($rowNom)) {
+                $wpdb->insert("{$wpdb->prefix}testNom", array('nom' => $nom));
+            }
+        }
+    }
+    public function add_admin_menu()
+
+    {
+        //on ajoute une page dans le menu administrateur  
+        add_menu_page('Hello World', 'Hello World plugin', 'manage_options', 'helloworld', array($this, 'menu_html'));
+    }
+    public function menu_html()
+    {
+        echo '<h1>' . get_admin_page_title() . '</h1>';
+    ?>
+        <form method="post" action="options.php">
+            <label>Couleur</label>
+            <input type="text" name="helloworld_couleur" value="<?php echo get_option("helloworld_couleur") ?>" />
+            <label>taille</label>
+            <input type="text" name="helloworld_taille" value="<?php echo get_option("helloworld_taille") ?>" />
+            <?php settings_fields('helloworld_settings') ?> 
+            <?php submit_button(); ?>
+        </form>
+    <?php
+    }
+
+    public function register_settings() 
+    {     
+        register_setting('helloworld_settings', 'helloworld_couleur'); 
+        register_setting('helloworld_settings', 'helloworld_taille'); 
+    }
+}
--- WpCréationWidget/wp-content/plugins/HelloWorld/helloWidget.php
+++ WpCréationWidget/wp-content/plugins/HelloWorld/helloWidget.php
@@ -14,10 +14,16 @@ class hellowidget extends WP_Widget
         echo apply_filters('widget_title', $instance['title']);
         echo $args['after_title'];
         // corps du widget  
-?>
-        <form action="" method="post">
+
+         // corps du widget
+         $couleur = get_option('helloworld_couleur', 'white'); 
+         $taille = get_option('helloworld_taille', '2em'); 
+         ?> 
+         <div id="test" style="color:<?php echo $couleur;?>">Hello World est un plug-in qui enregistre les commentaires en base de données</div>
+       
+         <form action="" method="post">
             <p>
-                <label for="helloworld_nom">Votre Nom :</label>
+                <label for="helloworld_nom" style="font-size:<?php echo $taille;?>">Votre Nom :</label>
                 <input id="helloworld_nom" name="helloworld_nom" type="texte" />
             </p>
             <p>
@@ -26,7 +32,18 @@ class hellowidget extends WP_Widget
             </p>
             <input type="submit" />
         </form>
-<?php
+    <?php
         echo $args['after_widget'];
+    }
+    public function form($instance)
+    // formulaire de gestion des paramètres pour le module d'administration 
+    {
+        $title = isset($instance['title']) ? $instance['title'] : '';
+    ?>
+        <p>
+            <label for="<?php echo $this->get_field_name('title'); ?>"><?php _e('Title:'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo  $title; ?>" />
+        </p>
+    <?php
     }
 }
