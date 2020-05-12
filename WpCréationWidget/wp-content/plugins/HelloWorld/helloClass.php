2c757e9a3200ee85cc353311fd057c531edba79b
Loicdom <loicdomDev@gmail.com>
2020-05-12 13:55:38 +0200

    MAJ exo Plugin

--- WpCréationWidget/wp-content/plugins/HelloWorld/helloClass.php
+++ WpCréationWidget/wp-content/plugins/HelloWorld/helloClass.php
@@ -7,7 +7,8 @@ class HelloClass{
         // on déclare le widget   
         add_action('widgets_init', function(){register_widget('HelloWidget');});  
         add_action('wp_enqueue_scripts',array($this,'persoCSS'),15); 
-        add_action('wp_loaded', array($this, 'save_comm'));
+        // on ajoute l'action de sauvegarde au chargement du widget   
+        add_action('wp_loaded', array($this, 'save_comm')); 
     } 
     function persoCSS() 
     {  
@@ -16,12 +17,12 @@ class HelloClass{
     public static function install() 
     {//méthode déclenchée à l'activation du plug-in     
         global $wpdb;     
-        $wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}helloworld_commentaire (id INT AUTO_INCREMENT PRIMARY KEY, comm VARCHAR(255) NOT NULL);"); 
+        $wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}testcomment (id INT AUTO_INCREMENT PRIMARY KEY, comments VARCHAR(255) NOT NULL);"); 
     }
     public static function uninstall()  
     {//méthode déclenchée à la suppression du module   
         global $wpdb;   
-        $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}helloworld_commentaire;");  
+        $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}testcomment;");  
     }
     public function save_comm() 
     {  
@@ -29,9 +30,9 @@ class HelloClass{
         {
             global $wpdb;
             $comm = $_POST['helloworld_comm'];
-            $row = $wpdb->get_row("SELECT * FROM {$wpdb->prefix}helloworld_commentaire WHERE comm = '$comm'");
+            $row = $wpdb->get_row("SELECT * FROM {$wpdb->prefix}testcomment WHERE comments = '$comm'");
             if (is_null($row)) {    
-                $wpdb->insert("{$wpdb->prefix}helloworld_commentaire", array('comm' => $comm));   
+                $wpdb->insert("{$wpdb->prefix}testcomment", array('comments' => $comm));   
             }  
         } 
     } 
