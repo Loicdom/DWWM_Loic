1506eaa9ad0de2991473276a46a500d89ee6a1e7
Loicdom <loicdomDev@gmail.com>
2020-12-05 18:32:42 +0100

    Stats maj

--- Projet Appli Stats/PHP/MODEL/ClientManager.Class.php
+++ Projet Appli Stats/PHP/MODEL/ClientManager.Class.php
@@ -1,48 +1,78 @@
 <?php
-class ClientManager
+class StatManager
 {
-    public static function add(Client $obj)
+    public static function add(Stat $obj)
     {
         var_dump($obj);
         $db = DbConnect::getDb();
-        $q = $db->prepare("INSERT INTO clients (idClient,nomClient,prenomClient,adresseClient,villeClient,cpClient) VALUES (:idClient,:nomClient,:prenomClient,:adresseClient,:villeClient,:cpClient)");
-        $q->bindValue(":idClient", $obj->getIdClient());
-        $q->bindValue(":nomClient", $obj->getNomClient());
-        $q->bindValue(":prenomClient", $obj->getPrenomClient());
-        $q->bindValue(":adresseClient", $obj->getAdresseClient());
-        $q->bindValue(":villeClient", $obj->getVilleClient());
-        $q->bindValue(":cpClient", $obj->getCpClient());
+        $q = $db->prepare("INSERT INTO stats (idStat,dateStat,minuteJouer,deuxPointTente,deuxPointMarque,troisPointTente,troisPointMarque,lancerFrancTente,lancerFrancMarque,rebondDef,rebondOff,passeDec,ballePerdue,interception,contreRealise,contreSubit,fautePerso,fauteProv,pointMarque,evaluation,idJoueur) VALUES (:IdStat,:dateStat,:minuteJouer,:deuxPointTente,:deuxPointMarque,:troisPointTente,:troisPointMarque,:lancerFrancTente,:lancerFrancMarque,:rebondDef,:rebondOff,:passeDec,:ballePerdue,:interception,:contreRealise,:contreSubit,:fautePerso,:fauteProv,:pointMarque,:evaluation,:idJoueur)");
+        $q->bindValue(":idStat", $obj->getIdStat());
+        $q->bindValue(":dateStat", $obj->getDateStat());
+        $q->bindValue(":minuteJouer", $obj->getMinuteJouer());
+        $q->bindValue(":deuxPointTente", $obj->getDeuxPointTente());
+        $q->bindValue(":deuxPointMarque", $obj->getDeuxPointMarque());
+        $q->bindValue(":troisPointTente", $obj->getTroisPointTente());
+        $q->bindValue(":troisPointMarque", $obj->getTroisPointMarque());
+        $q->bindValue(":lancerFrancTente", $obj->getLancerFrancTente());
+        $q->bindValue(":lancerFrancMarque", $obj->getLancerFrancMarque());
+        $q->bindValue(":rebondDef", $obj->getRebondDef());
+        $q->bindValue(":rebondOff", $obj->getRebondOff());
+        $q->bindValue(":passeDec", $obj->getPasseDec());
+        $q->bindValue(":ballePerdue", $obj->getBallePerdue());
+        $q->bindValue(":interception", $obj->getInterception());
+        $q->bindValue(":contreRealise", $obj->getContreRealise());
+        $q->bindValue(":contreSubit", $obj->getContreSubit());
+        $q->bindValue(":fautePerso", $obj->getFautePerso());
+        $q->bindValue(":fauteProv", $obj->getFauteProv());
+        $q->bindValue(":pointMarque", $obj->getPointMarque());
+        $q->bindValue(":evaluation", $obj->getEvaluation());
+        $q->bindValue(":idJoueur", $obj->getIdJoueur());
         $q->execute();
     }
 
-    public static function update(Client $obj)
+    public static function update(Stat $obj)
     {
         $db = DbConnect::getDb();
-        $q = $db->prepare("UPDATE clients SET  nomClient=:nomClient, prenomClient=:prenomClient, adresseClient=:adresseClient, villeClient=:villeClient, cpClient=:cpClient WHERE idclient=:idClient");
-        $q->bindValue(":idClient", $obj->getIdClient());
-        $q->bindValue(":nomClient", $obj->getNomClient());
-        $q->bindValue(":prenomClient", $obj->getPrenomClient());
-        $q->bindValue(":adresseClient", $obj->getAdresseClient());
-        $q->bindValue(":villeClient", $obj->getVilleClient());
-        $q->bindValue(":cpClient", $obj->getCpClient());
+        $q = $db->prepare("UPDATE stats SET  dateStat=:dateStat, minuteJouer=:minuteJouer, deuxPointTente=:deuxPointTente, deuxPointMarque=:deuxPointMarque, troisPointTente=:troisPointTente, troisPointMarque=:troisPointMarque, lancerFrancTente=:lancerFrancTente, lancerFrancMarque=:lancerFrancMarque, rebondDef=:rebondDef, rebondOff=:rebondOff, passeDec=:passeDec, ballePerdue=:ballePerdue, interception=:interception, contreRealise=:contreRealise, contreSubit=:contreSubit, fautePerso=:fautePerso, fauteProv=:fauteProv, pointMarque=:pointMarque, evaluation=:evaluation, idJoueur=:idJoueur WHERE IdStat=:IdStat");
+        $q->bindValue(":idStat", $obj->getIdStat());
+        $q->bindValue(":dateStat", $obj->getDateStat());
+        $q->bindValue(":minuteJouer", $obj->getMinuteJouer());
+        $q->bindValue(":deuxPointTente", $obj->getDeuxPointTente());
+        $q->bindValue(":deuxPointMarque", $obj->getDeuxPointMarque());
+        $q->bindValue(":troisPointTente", $obj->getTroisPointTente());
+        $q->bindValue(":troisPointMarque", $obj->getTroisPointMarque());
+        $q->bindValue(":lancerFrancTente", $obj->getLancerFrancTente());
+        $q->bindValue(":lancerFrancMarque", $obj->getLancerFrancMarque());
+        $q->bindValue(":rebondDef", $obj->getRebondDef());
+        $q->bindValue(":rebondOff", $obj->getRebondOff());
+        $q->bindValue(":passeDec", $obj->getPasseDec());
+        $q->bindValue(":ballePerdue", $obj->getBallePerdue());
+        $q->bindValue(":interception", $obj->getInterception());
+        $q->bindValue(":contreRealise", $obj->getContreRealise());
+        $q->bindValue(":contreSubit", $obj->getContreSubit());
+        $q->bindValue(":fautePerso", $obj->getFautePerso());
+        $q->bindValue(":fauteProv", $obj->getFauteProv());
+        $q->bindValue(":pointMarque", $obj->getPointMarque());
+        $q->bindValue(":evaluation", $obj->getEvaluation());
+        $q->bindValue(":idJoueur", $obj->getIdJoueur());
         $q->execute();
     }
 
-    public static function delete(Client $obj)
+    public static function delete(stat $obj)
     {
         $db = DbConnect::getDb();
-        $db->exec("DELETE FROM clients WHERE idclient=" . $obj->getIdclient());
+        $db->exec("DELETE FROM stats WHERE idStat=" . $obj->getIdStat());
     }
 
-    public static function findById($id)
+    public static function getById($id)
     {
         $db = DbConnect::getDb();
         $id = (int) $id;
-        $q = $db->query("SELECT * FROM clients WHERE idclient=$id");
+        $q = $db->query("SELECT * FROM stats WHERE IdStat=$id");
         $results = $q->fetch(PDO::FETCH_ASSOC);
         if ($results != false)
         {
-            return new Client($results);
+            return new Stat($results);
         }
         else
         {
@@ -53,16 +83,16 @@ class ClientManager
     public static function getList()
     {
         $db = DbConnect::getDb();
-        $clients = [];
-        $q = $db->query("SELECT * FROM clients");
+        $stats = [];
+        $q = $db->query("SELECT * FROM stats");
         while ($donnees = $q->fetch(PDO::FETCH_ASSOC))
         {
             if ($donnees != false)
             {
-                $clients[] = new Client($donnees);
+                $stats[] = new Stat($donnees);
             }
         }
-        return $clients;
+        return $stats;
     }
 
 }
